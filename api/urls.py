f6e6643f0e8c00dc5cad4400a193baeaaa3f2634
juanqui <juanquizh@gmail.com>
2025-05-14 20:16:26 -0500

    feat: agregar rutas de autenticación JWT en el archivo de URLs

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from .views import (
     CategoriaViewSet, EquipoViewSet, JugadorViewSet, JornadaViewSet,
     PartidoViewSet, GolViewSet, TarjetaViewSet, TorneoViewSet
 )
+from .auth import CustomTokenObtainPairView, RegistroUsuarioView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
@@ -17,4 +19,8 @@ router.register(r'torneos', TorneoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Rutas de autenticación JWT
+    path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/registro/', RegistroUsuarioView.as_view(), name='registro_usuario'),
 ]
